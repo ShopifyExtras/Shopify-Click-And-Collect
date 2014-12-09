02c59b6727e5519492cbdc8b20c9d0d822a3bdce
Andrew Cargill <andy@shopifyextras.com>
2014-12-09 08:24:48 +0000

    Update storepickup.js

--- basic/storepickup.js
+++ basic/storepickup.js
@@ -1,31 +1,76 @@
 $( document ).ready(function() {
+ 
+  // Populate locations select field on page load.
+  getStoreLocations();
   
-  $("#savePickupLocation").on("click", function() {
-	         
-      if($("#selectedStorePickupId").val() != '') {
+  function getStoreLocations() 
+  {
+       // Get the Click and Collect item from your store. 
+       jQuery.getJSON('/products/click-and-collect.js', function(data) {  
+       	
+         	// We only need the variants.
+         	var selectValues = data.variants;
+         	
+         	// Add each variant (location) to the select field. 
+         	$.each(selectValues, function(id, variant) { 
+              
+                jQuery("#clickAndCollectVariantId")
+                    .append($("<option></option>")
+                    .attr("value", variant.id)
+                    .text(variant.title)); 
+            
+
+            });
+       
+         	// Get the selected location ID.
+            var variantid = $("#selectedClickAndCollectId").val();
+         
+            // Set the selected location in the select field.
+            jQuery("#clickAndCollectVariantId").val(variantid);
+       });
+  }
+  
+  
+  function removeClickAndCollectItem()
+  {
+     if($("#selectedClickAndCollectId").val() != '') {
+       
         var item = {};
-        item[$("#selectedStorePickupId").val()] = 0;
+        item[$("#selectedClickAndCollectId").val()] = 0;
           
         // Remove existing item. 
         var response = jQuery.post('/cart/update.js', { "updates": item });
 
         // Remove existing_pickup_id. 
-        $("#selectedStorePickupId").val("");
+        $("#selectedClickAndCollectId").val("");
       } 
+  }
+
+  $("#saveClickAndCollectLocation").on("click", function() {
+	  
+      // Remove the existing Click & Collect item from the basket.
+      removeClickAndCollectItem();
     
-      if($("#storePickupVariantId option:selected").val() != "") {
-          
+      var clickAndCollectVariantId = $("#clickAndCollectVariantId option:selected").val();
+    
+      if(clickAndCollectVariantId != "") {
+
           var item = {
-              quantity: 1,
-              id: $("#storePickupVariantId option:selected").val()
+            quantity: 1,
+            id: clickAndCollectVariantId
           }
 
-          var response = jQuery.post('/cart/add.js', item);   
+          $.ajax({
+              url: "/cart/add.js",
+              type: 'POST',
+              data: item,
+              complete: function(msg){ 
+                  window.location.replace("/cart");
+              }
+          });
         
-          console.log(response);
-          alert("Pickup location set.");
-        
-      } 
+      }
+    
       return false;
       
   });
